ad93ae0044f0b2318c6d3e3b96d86210bbbc08fc
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-09-21 10:00:44 +0200

    ci: add release note

--- server/system/trpc.ts
+++ server/system/trpc.ts
@@ -1,6 +1,9 @@
-import { procedure, router } from '~~/server/api'
-import { reboot } from '~~/server/system/command'
+import { procedure, router } from '~~/server/api';
+import { reboot } from '~~/server/system/command';
+import { downloadLatestOtaAsset, fetchLatestOtaRelease } from '~~/server/system/ota';
 
 export const system = router({
-  reboot: procedure.mutation(() => reboot()),
-})
+	reboot: procedure.mutation(() => reboot()),
+	latestOtaRelease: procedure.query(() => fetchLatestOtaRelease()),
+	downloadLatestOtaAsset: procedure.mutation(() => downloadLatestOtaAsset()),
+});
