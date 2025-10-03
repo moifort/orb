9593951d2952973926cd0df1c5ccc2d2a7fe8942
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-10-03 19:11:47 +0200

    refactor: move trpc instance into utils

--- software/server/domains/heater/infra/sensor.data.ts
+++ software/server/domains/heater/infra/sensor.data.ts
@@ -2,7 +2,6 @@ import data from './sensor.data.json'
 
 let index = 0
 export const getFakeHeaterSensorData = () => {
-  if (index < data.length) return data[index++]
-  index = 0
-  return data[index]
+  if (index === data.length) index = 0
+  return data[index++] ?? '00000000'
 }
