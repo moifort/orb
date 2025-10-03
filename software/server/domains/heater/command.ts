dd101f10c6a7037fcaee01e5b15ffed14fc77700
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-10-03 18:53:25 +0200

    refactor: move trpc instance into utils

--- software/server/domains/heater/command.ts
+++ software/server/domains/heater/command.ts
@@ -3,7 +3,6 @@ import type { CurrentTemperatureC } from '#shared/heater/type'
 export namespace HeaterCommand {
   export type Store = { 'heater:temperatures': CurrentTemperatureC[] }
   export type Events = { 'heater:currentTemperature': CurrentTemperatureC }
-
   export const saveTemperature =
     (storage: TypedStorage, eventBus: EventBus) => async (temperature: CurrentTemperatureC) => {
       const temperatures = ((await storage.getItem('heater:temperatures')) || []).slice(-99)
